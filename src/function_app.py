77662fb38b4e82c43b967346437340b9303e6fbb
Maurice Künicke <m.kuenicke1995@gmail.com>
2023-09-25 16:12:54 +0200

    Initial Error Handling Added

--- src/function_app.py
+++ src/function_app.py
@@ -25,10 +25,10 @@ def dump_to_local_toml(content: str) -> None:
     return None
 
 
-def create_result_base_path() -> str:
+def create_result_base_path(file_root: str) -> str:
     current_date = datetime.now().strftime("%Y%m%d")
     current_date_time = datetime.now().strftime("%Y%m%d%H%M")
-    run_folder_name = f"{current_date_time}_my_test"
+    run_folder_name = f"{current_date_time}_{file_root}"
     base_path = os.path.join(current_date, run_folder_name)
     return base_path
 
@@ -41,46 +41,75 @@ def test_function(newblob: func.InputStream):
     # ATTACH FILE HANDLER
     logging.getLogger().addHandler(create_file_handler())
 
-    content = read_blob_content(newblob)
+    # INIT
     logging.info(f"Python blob trigger function processed blob: {newblob.name}")
-    logging.info(f"Configuration file content:\n{content}")
 
-    dump_to_local_toml(content)
+    # SETUP
+    logging.info("===============================================================================================")
+    logging.info("=========================================SETUP=================================================")
+    logging.info("===============================================================================================")
+    file_name = os.path.basename(newblob.name)
+    logging.info(f"File name set to: {file_name}")
+
+    file_root, file_extension = os.path.splitext(file_name)
+    logging.info(f"File root: {file_root}")
+    logging.info(f"File extension: {file_extension}")
+
+    result_base_path = create_result_base_path(file_root)
+    logging.info(f"Result base path set to: {result_base_path}")
 
     connection_string = os.environ["StorageConnection"]
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-    result_base_path = create_result_base_path()
+    logging.info("Blob Service Client received.")
 
     # Create a ContainerClient
     container_client = blob_service_client.get_container_client(RESULT_CONTAINER_NAME)
-
-    # Create or overwrite the file in Blob Storage
-    blob_client = container_client.get_blob_client(result_base_path+"/config.toml")
-    blob_client.upload_blob(content, overwrite=True)
-
-    # Log the successful upload
-    logging.info(f"Uploaded result files to '{result_base_path}' container")
-
-    # Clean Ingest Stage
+    logging.info("Result Container Client received.")
     ingest_container_client = blob_service_client.get_container_client(INGEST_CONTAINER_NAME)
-    ingest_container_client.delete_blob("test.toml")
-    logging.info(f"{newblob.name} successfully deleted from ingest area.")
-    logging.info(f"Upload and remove '{LOG_FILE_PATH}'.")
+    logging.info("Ingest Container Client received.")
 
-    with open(LOG_FILE_PATH, "rb") as log_file:
-        blob_client = container_client.get_blob_client(result_base_path+"/execution.log")
-        blob_client.upload_blob(log_file, overwrite=True)
+    try:
+        # VALIDATE TOML
+        logging.info("===============================================================================================")
+        logging.info("=====================================VALIDATE CONFIG===========================================")
+        logging.info("===============================================================================================")
+        content = read_blob_content(newblob)
+        logging.info(f"Configuration file content:\n{content}\n")
+        dump_to_local_toml(content)
 
-    for handler in logging.getLogger().handlers:
-        if isinstance(handler, logging.FileHandler):
-            handler.close()
+        # Create or overwrite the file in Blob Storage
+        blob_client = container_client.get_blob_client(result_base_path+"/config.toml")
+        blob_client.upload_blob(content, overwrite=True)
 
-    # CLEAR LOCAL FILES
-    if True:
-        logging.getLogger().removeHandler(create_file_handler())
-        if os.path.exists(LOG_FILE_PATH):
-            os.remove(LOG_FILE_PATH)
+        # Log the successful upload
+        logging.info(f"Uploaded result files to '{result_base_path}' container")
 
-        if os.path.exists(TOML_FILE_PATH):
-            os.remove(TOML_FILE_PATH)
+    except Exception as e:
+        logging.error(e)
+
+    finally:
+        logging.info("===============================================================================================")
+        logging.info("=======================================CLEAN UP================================================")
+        logging.info("===============================================================================================")
+
+        # Clean Ingest Stage
+        ingest_container_client.delete_blob(file_name)
+        logging.info(f"{newblob.name} successfully deleted from ingest area.")
+        logging.info(f"Upload and remove '{LOG_FILE_PATH}'.")
+
+        with open(LOG_FILE_PATH, "rb") as log_file:
+            blob_client = container_client.get_blob_client(result_base_path+"/execution.log")
+            blob_client.upload_blob(log_file, overwrite=True)
+
+        for handler in logging.getLogger().handlers:
+            if isinstance(handler, logging.FileHandler):
+                handler.close()
+
+        # CLEAR LOCAL FILES
+        if True:
+            logging.getLogger().removeHandler(create_file_handler())
+            if os.path.exists(LOG_FILE_PATH):
+                os.remove(LOG_FILE_PATH)
+
+            if os.path.exists(TOML_FILE_PATH):
+                os.remove(TOML_FILE_PATH)
